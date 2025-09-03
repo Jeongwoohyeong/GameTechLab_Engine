b1db36c97672a754f059cd795d680af512026cf4
WH <121390020+Jeongwoohyeong@users.noreply.github.com>
2025-09-03 18:34:57 +0900

    sprite json 파서 추가

---
+++ pikkachubaegoo/pikkachubaegoo/SpriteSheet.cpp
@@ -0,0 +1,111 @@
+#include <fstream>
+#include <iostream>
+#include "json.hpp"
+#include "SpriteSheet.h"
+
+USpriteSheet::USpriteSheet()
+    :texture(nullptr)
+{
+}
+
+USpriteSheet::~USpriteSheet()
+{
+}
+
+bool USpriteSheet::Load(std::string jsonPath)
+{
+    using json = nlohmann::json;
+    spriteFramePath = jsonPath;
+
+    std::ifstream fp(spriteFramePath);
+    if (!fp.is_open())
+    {
+        std::cerr << "file open fail";
+        return false;
+    }
+
+    json frameJson;
+    fp >> frameJson;
+
+    // 메타데이터 추출
+    if (frameJson.contains("meta"))
+    {
+        // 디렉토리 경로 추출
+        std::string directoryPath = "";
+        size_t slashPosition = spriteFramePath.find_last_of("/\\");        
+        if (slashPosition != std::string::npos)
+        {
+            directoryPath = spriteFramePath.substr(0, slashPosition + 1);
+        }
+
+        const auto& meta = frameJson["meta"];
+
+        std::string imageFileName = meta["image"];
+        metaData.imagePath = directoryPath + imageFileName;    
+        metaData.size = {
+            meta["size"]["w"],
+            meta["size"]["h"]
+        };
+        metaData.scale = std::stof(meta["scale"].get<std::string>());
+    }
+    
+    // spriteframe 추출
+    for (auto& item : frameJson["frames"].items())
+    {
+        const std::string& key = item.key();
+        const auto& value = item.value();
+
+        FSpriteFrame newFrame;
+        newFrame.name = key;
+        
+        if (value.contains("frame"))
+        {
+            const auto& data = value["frame"];
+            newFrame.position = { data["x"], data["y"] };
+            newFrame.size = { data["w"], data["h"] };
+        }
+        
+        if (value.contains("rotated"))
+        {
+            newFrame.isRotate = value["rotated"].get<bool>();
+        }
+
+        if (value.contains("trimmed"))
+        {
+            newFrame.isTrim = value["trimmed"].get<bool>();
+        }
+
+        if (value.contains("spriteSourceSize"))
+        {            
+            newFrame.spriteSourceSize = { 
+                value["spriteSourceSize"]["x"],
+                value["spriteSourceSize"]["y"],
+                value["spriteSourceSize"]["w"],
+                value["spriteSourceSize"]["h"]
+            };
+        }
+
+        if (value.contains("sourceSize"))
+        {
+            newFrame.sourceSize = { value["sourceSize"]["w"], value["sourceSize"]["h"] };
+
+        }
+
+        frameDatas[key] = newFrame;
+    }
+
+    return true;
+}
+
+const FSpriteFrame* USpriteSheet::GetFrame(const std::string& spriteName) const
+{
+    std::map<std::string, FSpriteFrame>::const_iterator it = frameDatas.find(spriteName);
+
+    if (it == frameDatas.end())
+    {
+        std::cerr << "invalid spritename";
+        return nullptr;
+    }
+
+    return &it->second;
+}
--- pikkachubaegoo/pikkachubaegoo/SpriteSheet.h
+++ pikkachubaegoo/pikkachubaegoo/SpriteSheet.h
@@ -1,6 +1,19 @@
 #pragma once
+#include "SpriteStruct.h"
+#include <map>
 
 class USpriteSheet
 {
+public:
+	USpriteSheet();
+	~USpriteSheet();	
+	bool Load(std::string jsonPath);
+	const FSpriteFrame* GetFrame(const std::string& spriteName) const;
+	const FSpriteMeta* GetMetaData() const { return &metaData; }
 
+private:
+	FSpriteMeta metaData;
+	std::string spriteFramePath;
+	std::map<std::string, FSpriteFrame> frameDatas;
+	void* texture;
 };
