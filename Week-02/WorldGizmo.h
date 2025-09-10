1de0e5db79ea8dadcf331b3ea7984c172000d6c4
Soyeon Joo <sildredel@naver.com>
2025-09-10 23:38:48 +0900

    WIP

--- Week-02/WorldGizmo.h
+++ Week-02/WorldGizmo.h
@@ -43,7 +43,7 @@ private:
     // 색상 프리셋
     FVertexSimple Minor = { 0,0,0,  0.55f,0.55f,0.55f,1.0f };
     FVertexSimple Major = { 0,0,0,  0.30f,0.30f,0.30f,1.0f };
-    FVertexSimple YColor = { 0,0,0,  0.4f,1.0f,0.4f,1.0f };
-    FVertexSimple ZColor = { 0,0,0,  0.2f,0.4f,1.0f,1.0f };
-    FVertexSimple XColor = { 0,0,0,  1.0f,0.2f,0.2f,1.0f };
+    FVertexSimple ZColor = { 0,0,0,  0.4f,1.0f,0.4f,1.0f };
+    FVertexSimple XColor = { 0,0,0,  0.2f,0.4f,1.0f,1.0f };
+    FVertexSimple YColor = { 0,0,0,  1.0f,0.2f,0.2f,1.0f };
 };
