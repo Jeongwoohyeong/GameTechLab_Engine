a2d9d36a1d9ef3e6f897e2c4120fdf6f76f5fc45
mcminchan1021 <mcminchan1021@gmail.com>
2025-09-20 22:03:39 +0900

    StaticShader가 쓸데없이 Color값을 Contant Buffer, Instance Buffer, Vertex Buffer로부터 받고 있던 것 수정. 수정했더니 StaticMeshComponent도 정상적으로 렌더링됨. 그리고 기존의 셰이더 SampleShader를 그리드와 축, 기즈모 출력용으로 단순한 코드로 수정했고, 이로써 InstanceConstant가 필요없어져서 StaticShader, SampleShader에서 모두 제거하고 Renderer에서도 제거함.

--- Engine/Asset/Shader/SampleShader.hlsl
+++ Engine/Asset/Shader/SampleShader.hlsl
@@ -16,29 +16,6 @@ cbuffer PerDrawColor : register(b2)
     float4 totalColor;
 };
 
-cbuffer InstanceParams : register(b3)
-{
-	uint UseInstancing;
-}
-
-//아래의 constantbuffer는 여러 종류의 매시를 그릴 때 스트럭처드 버퍼를 한번만 업데이트해서 그리기 위해 존재함.
-//지금은 매시 종류가 10가지도 안되므로 오버엔지니어링이라고 생각해서 주석처리함.
-//cbuffer InstanceParams : register(b3)
-//{
-//	uint UseInstancing;
-//	uint BaseInstanceOffset; //baseOffset
-//	uint InstanceCount; //인스턴스 개수
-//	uint Padding0;
-//};
-
-struct InstanceData
-{
-    row_major float4x4 World;
-    float4 Color;
-};
-
-StructuredBuffer<InstanceData> InstanceMatrices : register(t0);
-
 struct VS_INPUT
 {
     float4 Position : POSITION;
@@ -51,40 +28,19 @@ struct PS_INPUT
     float4 Color : COLOR;
 };
 
-//InstanceID는 GPU가 알아서 세팅해줌.(CPU에서 넘긴 인스턴스 버퍼의 데이터 순서대로 0부터 InstanceCount까지 알아서 전달)
-//SV_InstanceID 태그로 식별
-PS_INPUT MainVS(VS_INPUT Input, uint InstanceId : SV_InstanceID)
+
+PS_INPUT MainVS(VS_INPUT Input)
 {
     PS_INPUT Output;
 
     float4 Position = Input.Position;
-    float4 ShadeColor = Input.Color;
-
-	
-	//아래 주석은 InstanceParam 확인
-    //if (UseInstancing != 0 && InstanceId < InstanceCount)
-    //{
-    //    InstanceData Instance = InstanceMatrices[BaseInstanceOffset + InstanceId];
-    //    Position = mul(Position, Instance.World);
-    //    ShadeColor = lerp(ShadeColor, Instance.Color, Instance.Color.a);
-    //}
-
-	//현재 에디터 라인 배치를 할때 같은 셰이더를 쓰기 때문에 Instancing을 사용하는지 확인이 필요함.
-	//나중에 라인 배치는 다른 셰이더를 사용하고 가독성을 위해 아래 코드를 지우는 게 나을 것 같음.
-	if(UseInstancing != 0)
-	{
-		InstanceData Instance = InstanceMatrices[InstanceId];
-		Position = mul(Position, Instance.World);
-		ShadeColor = lerp(ShadeColor, Instance.Color, Instance.Color.a);
-	}
 		
-	//인스턴싱 하는 경우 어차피 World는 Identity임.
     Position = mul(Position, world);
     Position = mul(Position, ViewMatrix);
     Position = mul(Position, ProjectionMatrix);
 
     Output.Position = Position;
-    Output.Color = ShadeColor;
+    Output.Color = Input.Color;
     return Output;
 }
 
--- Engine/Public/Render/Renderer/Renderer.h
+++ Engine/Public/Render/Renderer/Renderer.h
@@ -126,12 +126,6 @@ public:
 	void UpdateConstant(const FViewProjConstants& InViewProjConstants) const;
 	void UpdateConstant(const FVector4& Color) const;
 	void UpdateInstance(const TArray<FTextInstance>* Instance);
-	//아래 함수는 여러 종류의 매시를 그릴 때 스트럭처드 버퍼를 한번만 업데이트해서 그리기 위해 존재하는 코드임.
-	//지금은 매시 종류가 10가지도 안되므로 오버엔지니어링이라고 생각해서 주석처리함.
-	//void UpdateInstanceDrawConstants(bool bUseInstancing, uint32 BaseInstanceOffset, uint32 InstanceCount) const;
-	
-	//인스턴싱을 할지 말지 결정해줌
-	void UpdateInstanceDrawConstants(bool bUseInstancing) const;
 
 	void SetViewMode(EViewModeIndex InViewMode) { CurrentViewMode = InViewMode; }
 	EViewModeIndex GetViewMode(EViewModeIndex InViewMode) const { return CurrentViewMode; }
@@ -177,9 +171,6 @@ private:
 	ID3D11Buffer* ConstantBufferPerFrame = nullptr;
 	ID3D11Buffer* ConstantBufferColor = nullptr;
 	ID3D11Buffer* ConstantBufferCharTable = nullptr;
-	//인스턴싱을 위한 constant buffer(default pass에 대한)
-	ID3D11Buffer* ConstantBufferInstance = nullptr;
-	//////////////////////////////////////
 
 	ID3D11Buffer* TextInstanceBuffer = nullptr;
 	/////////////////////////////////////
@@ -208,42 +199,6 @@ private:
 	uint32 Stride = 0;
 	uint32 StrideTextVertex = 0;
 	uint32 StrideTextInstance = 0;
-
-	//struct FPrimitiveBatchKey
-	//{
-	//	ID3D11Buffer* VertexBuffer = nullptr;
-	//	ID3D11Buffer* IndexBuffer = nullptr;
-	//	uint32 IndexCount = 0;
-	//	FRenderState RenderState = {};
-	//	bool operator==(const FPrimitiveBatchKey& InRhs) const	//위의 모든 멤버가 같아야 같은 키
-	//	{
-	//		return VertexBuffer == InRhs.VertexBuffer &&
-	//			IndexBuffer == InRhs.IndexBuffer &&
-	//			IndexCount == InRhs.IndexCount &&
-	//			RenderState.CullMode == InRhs.RenderState.CullMode &&
-	//			RenderState.FillMode == InRhs.RenderState.FillMode;
-	//	}
-	//};
-
-	//struct FPrimitiveBatchKeyHasher
-	//{
-	//	size_t operator()(const FPrimitiveBatchKey& InKey) const noexcept
-	//	{
-	//		auto Mix = [](size_t& H, size_t V)
-	//		{
-	//			H ^= V + 0x9e3779b97f4a7c15ULL + (H << 6) + (H >> 2);
-	//		};
-
-	//		size_t Hash = 0;
-	//		Mix(Hash, reinterpret_cast<size_t>(InKey.VertexBuffer));
-	//		Mix(Hash, reinterpret_cast<size_t>(InKey.IndexBuffer));
-	//		Mix(Hash, static_cast<size_t>(InKey.IndexCount));
-	//		Mix(Hash, static_cast<size_t>(InKey.RenderState.CullMode));
-	//		Mix(Hash, static_cast<size_t>(InKey.RenderState.FillMode));
-
-	//		return Hash;
-	//	}
-	//};
 
 	struct FStructuredBufferResource
 	{
