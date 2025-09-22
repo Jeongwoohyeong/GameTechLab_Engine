b3973e65cadb7c4c3c734835602f844d14bec4c9
mcminchan1021 <mcminchan1021@gmail.com>
2025-09-22 17:24:12 +0900

    쓸데없는 코드 제거, 씬 생성 후 StaticMesh 변경 시 Assert호출되는 현상 픽스

--- Engine/Public/Mesh/VertexDatas.h
+++ Engine/Public/Mesh/VertexDatas.h
@@ -1,20 +1,11 @@
 #pragma once
-
-extern TArray<FVertex> VerticesSphere;
-extern TArray<FVertex> VerticesCube;
-extern TArray<FVertex> VerticesTriangle;
-extern TArray<FVertex> VerticesSquare;
 extern TArray<FVertex> VerticesArrow;
 extern TArray<FVertex> VerticesCubeArrow;
 extern TArray<FVertex> VerticesRing;
 extern TArray<FTextVertex> VerticesText;
 
-extern TArray<FVertex> ReducedVerticesSphere;
-extern TArray<FVertex> ReducedVerticesCube;
-extern TArray<FVertex> ReducedVerticesTriangle;
-extern TArray<FVertex> ReducedVerticesSquare;
+
 extern TArray<FVertex> ReducedVerticesArrow;
 extern TArray<FVertex> ReducedVerticesCubeArrow;
 extern TArray<FVertex> ReducedVerticesRing;
 
-extern TArray<FVertex> objcube;
