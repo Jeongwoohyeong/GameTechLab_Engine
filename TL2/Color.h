1451b18b52e70e8c0217ff3545e583bce5211209
sb-111 <117798778+sb-111@users.noreply.github.com>
2025-10-14 16:18:53 +0900

    fix: Color.h의 FLinearColor에 연산자 옮김

--- TL2/Color.h
+++ TL2/Color.h
@@ -16,6 +16,54 @@ struct FLinearColor
     static const FLinearColor Red;
     static const FLinearColor Green;
     static const FLinearColor Blue;
+
+    // ======== Operator =======
+    FLinearColor operator+(const FLinearColor& Other) const
+    {
+        return FLinearColor(R + Other.R, G + Other.G, B + Other.B, A + Other.A);
+    }
+    FLinearColor operator-(const FLinearColor& Other) const
+    {
+        return FLinearColor(R - Other.R, G - Other.G, B - Other.B, A - Other.A);
+    }
+    FLinearColor operator*(const FLinearColor& Other) const
+    {
+        return FLinearColor(R * Other.R, G * Other.G, B * Other.B, A * Other.A);
+    }
+    FLinearColor operator*(float Scalar) const
+    {
+        return FLinearColor(R * Scalar, G * Scalar, B * Scalar, A * Scalar);
+    }
+    FLinearColor operator/(float Scalar) const
+    {
+        const float InvScalar = 1.0f / Scalar;
+        return FLinearColor(R * InvScalar, G * InvScalar, B * InvScalar, A * InvScalar);
+    }
+    FLinearColor& operator+=(const FLinearColor& Other)
+    {
+        R += Other.R; G += Other.G; B += Other.B; A += Other.A;
+        return *this;
+    }
+    FLinearColor& operator*=(float Scalar)
+    {
+        R *= Scalar; G *= Scalar; B *= Scalar; A *= Scalar;
+        return *this;
+    }
+    bool operator==(const FLinearColor& Other) const
+    {
+        // Use a small tolerance (epsilon) for robust float comparison
+        return std::abs(R - Other.R) < 1e-4f &&
+            std::abs(G - Other.G) < 1e-4f &&
+            std::abs(B - Other.B) < 1e-4f &&
+            std::abs(A - Other.A) < 1e-4f;
+    }
+    bool operator!=(const FLinearColor& Other) const
+    {
+        return !(*this == Other);
+    }
+    // ========== Utility ==========
+
+};
 };
 
 inline const FLinearColor FLinearColor::White{ 1.0f, 1.0f, 1.0f, 1.0f };
