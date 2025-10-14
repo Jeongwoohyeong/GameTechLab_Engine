46e5133374049a55dd963777c8ceca63c89787fe
sb-111 <117798778+sb-111@users.noreply.github.com>
2025-10-14 16:13:38 +0900

    fix: 임시 주석 처리
    TODO: Rendering 기능 추가 시 활성화 예정

--- TL2/FireBallComponent.cpp
+++ TL2/FireBallComponent.cpp
@@ -7,13 +7,13 @@ UFireBallComponent::UFireBallComponent() : Intensity(10.f), Radius(50.f), Radius
 
 void UFireBallComponent::Render(URenderer* Renderer, const FMatrix& View, const FMatrix& Proj)
 {
-	FVector4 WorldLocation(GetWorldLocation().X, GetWorldLocation().Y, GetWorldLocation().Z, 1.0f);
+	/*FVector4 WorldLocation(GetWorldLocation().X, GetWorldLocation().Y, GetWorldLocation().Z, 1.0f);
 	FVector4 Parameters(Intensity, Radius, 1.f / Radius, RadiusFallOff);
 	FireBallBufferType FireBallData = { WorldLocation, Color, Parameters };
 
 	Renderer->UpdateConstantBuffer(GetWorldMatrix(), View, Proj);
 	Renderer->UpdateFireBallConstantBuffer(FireBallData);
-	Renderer->PrepareShader(Super_t::GetMaterial()->GetShader());
+	Renderer->PrepareShader(Super_t::GetMaterial()->GetShader());*/
 	/*Renderer->DrawIndexedPrimitiveComponent(
 		
 	)*/
