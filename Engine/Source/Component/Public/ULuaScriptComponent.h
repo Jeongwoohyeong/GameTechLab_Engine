a7ddbf968b2b4efc7a2f9b2a936539f3611fcd56
TeshShin <shintom1222@gmail.com>
2025-11-05 04:58:01 +0900

    fix

--- Engine/Source/Component/Public/ULuaScriptComponent.h
+++ Engine/Source/Component/Public/ULuaScriptComponent.h
@@ -55,35 +55,24 @@ public:
 template<typename ...Args>
 inline void ULuaScriptComponent::ActivateFunction(const FString& FunctionName, Args && ...args)
 {
-    // FString을 std::string으로 명시적 변환 (Release 빌드 안정성)
-    std::string funcName = FunctionName.c_str();
-
-    if (SelfTable.valid())
+    if (SelfTable.valid() && SelfTable[FunctionName].valid())
     {
-        // sol::table 인덱스 접근을 std::string으로
-        sol::object funcObj = SelfTable[funcName];
-        if (funcObj.valid() && funcObj.is<sol::function>())
+        //UE_LOG("[ULuaScriptComponent] ActivateFunction: Calling '%s'", FunctionName.c_str());
+        auto Result = SelfTable[FunctionName](SelfTable, std::forward<Args>(args)...);
+        if (!Result.valid())
         {
-            //UE_LOG("[ULuaScriptComponent] ActivateFunction: Calling '%s'", funcName.c_str());
-            sol::function func = funcObj;
-            auto Result = func(SelfTable, std::forward<Args>(args)...);
-            if (!Result.valid())
-            {
-                sol::error err = Result;
-                UE_LOG_ERROR("[ULuaScriptComponent] Lua function '%s' failed: %s", funcName.c_str(), err.what());
-            }
-            else
-            {
-                //UE_LOG("[ULuaScriptComponent] ActivateFunction: '%s' executed successfully", funcName.c_str());
-            }
+            sol::error err = Result;
+            UE_LOG_ERROR("[ULuaScriptComponent] Lua function '%s' failed: %s", FunctionName.c_str(), err.what());
         }
         else
         {
-            UE_LOG_ERROR("[ULuaScriptComponent] ActivateFunction: Function '%s' not valid in table", funcName.c_str());
+            //UE_LOG("[ULuaScriptComponent] ActivateFunction: '%s' executed successfully", FunctionName.c_str());
         }
     }
     else
     {
-        UE_LOG_ERROR("[ULuaScriptComponent] ActivateFunction: SelfTable is not valid");
+        UE_LOG_ERROR("[ULuaScriptComponent] ActivateFunction: Function '%s' not valid in table", FunctionName.c_str());
+
     }
+
 }
